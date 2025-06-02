5b5161afdb49cb5941c6e4ccdbd8117bd76805fb
KuRa04 <daichi-asakura@cybozu.co.jp>
2025-06-03 08:12:40 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	nStr := r.URL.Query().Get("n")
+	n, err := strconv.Atoi(nStr)
+	if err != nil || n < 1 {
+		http.Error(w, "パラメータ'n'は1以上の整数で指定してください", http.StatusBadRequest)
+		return
+	}
+
+	result := make([]string, n)
+	for i := 0; i < n; i++ {
+		result[i] = fmt.Sprintf("item%d", i+1)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result)
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
